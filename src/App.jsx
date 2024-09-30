60c132852c11ab92039a66cf1150757f266a5c68
Padmajothi <pathimoolam@prometheussoftware.ca>
2024-09-30 10:10:47 -0400

    initial setup and Header component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './sections/header'
+
+const App = () => {
+  return (
+    <main className="overflow-hidd">
+      <Header />
+    </main>
+  )
+}
+
+export default App
