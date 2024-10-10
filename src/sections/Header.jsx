77d45e43d1e804c180f4c2988389dd640c58234f
Padmajothi <pathimoolam@prometheussoftware.ca>
2024-10-10 10:43:53 -0400

    Updated Hero component

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link as LinkScroll } from 'react-scroll';
+import clsx from 'clsx';
 
 const NavLink =({ title }) => (  //immediate return function
     <LinkScroll className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
@@ -16,7 +17,7 @@ const Header = () => {
             <a className='flex-1 cursor-pointer z-2'>
                 <img src="/images/landify.svg" width={400} height={400} alt="logo"/>
             </a>
-            <div className='w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0' >
+            <div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0',isOpen ? "max-lg:opacity-100": "max-lg:poin" ) }>
                 <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
                     <nav className='max-lg:relative max-lg:z-2 max-lg:my-auto'>
                         <ul className='flex max-lg:block max-lg:px-12'>
@@ -26,7 +27,7 @@ const Header = () => {
                                 <NavLink title="pricing" />
                             </li>
                             <li className='nav-logo'>
-                                <LinkScroll>
+                                <LinkScroll to="hero" offset={-100} spy smooth className={clsx("max-lg:hidden transition-transform duration-500 ursor-pointer")}>
                                 <img src="/images/landify.svg" width={160} height={55} alt="logo"/>
                                 </LinkScroll>
                             </li>
@@ -37,6 +38,10 @@ const Header = () => {
                             </li>
                         </ul>
                     </nav>
+                    <div className='lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90'>
+                        <img src="/images/bg-outlines.svg" width={960} height={380} alt="outline" className='relative z-2' />
+                        <img src="/images/bg-outlines-fill.png" width={960} height={380} alt="outline" className='absolute inset-0 mix-blend-soft-light opacity-5' />
+                    </div>
                 </div>
             </div>
             <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center" onClick={() => setIsOpen((prevState) => !prevState)}>
